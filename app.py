373485801b2c72a2c4b4a41bea29a43f20bef1a4
INFINITE0524 <78087228+INFINITE0524@users.noreply.github.com>
2024-08-18 00:15:46 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///APB.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 class Appointment(db.Model):
     __tablename__ = 'AP00'
