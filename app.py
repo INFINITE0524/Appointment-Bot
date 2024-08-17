2845589b167859db82c631b8f170e7b85926f0d5
INFINITE0524 <78087228+INFINITE0524@users.noreply.github.com>
2024-08-17 15:46:35 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/appointment')
 def home():
-    return render_template('home.html')
+    return render_template('appointment.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
